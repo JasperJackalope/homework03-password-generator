11533f4884cd7814122ed9e5d8c1b511055a94fa
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-03-12 13:20:32 -0700

    JS note and definitions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,30 @@
 // Assignment Code
+// main assignment is to define the fucntion writePassword 
 var generateBtn = document.querySelector("#generate");
+// above targets the generate button, event listener added at the end for the even on click.
 
-// Write password to the #password input
+function generatePassword () {
+  console.log("Click clack gurl");
+
+  return "Password TBD"
+  }
+
+// Write password to the #password input. 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+
   passwordText.value = password;
+  // takes value of generated password and displays it on screen
 
 }
 
 // Don't touch above. Write all code below
 
-function generatePassword () {
+// function generatePassword () {
 
-}
+// }
 
-// Add event listener to generate button
+// Add event listener to generate button. below calls the writePasswork function stared above. need to write the writePassword function. 
 generateBtn.addEventListener("click", writePassword);
