e14955eef1850014438523ed39a60c80d55b3e3f
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-03-12 14:14:44 -0700

    Defined Prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@
 
 var characterLength = 8;
 var choiceArray = [];
+
 var specialCharacter = ["!","@","#","$","^","&","*","(",")","-","=","+","_","`","~","?"];
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","J","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -11,6 +12,21 @@ var numberArray = ["1","2","3","4","5","6","7","8","9","0"];
 
 var generateBtn = document.querySelector("#generate");
 // above targets the generate button, event listener added at the end for the even on click.
+
+generateBtn.addEventListener("click", writePassword);
+
+
+
+// Write password to the #password input. 
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+
+  passwordText.value = password;
+  // takes value of generated password and displays it on screen
+
+}
 
 function generatePassword () {
   console.log("Click clack gurl");
@@ -27,17 +43,27 @@ function generatePassword () {
   // shows that the event is connect and working. at the very end, need to return something, store generated passwork into a variable and return that. 
   }
 
-// Write password to the #password input. 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-
-  passwordText.value = password;
-  // takes value of generated password and displays it on screen
+function promptAlert (){
+  characterLength = parseInt(prompt("How long should the password be? Please choose between 8-128 characters."));
+  if (isNaN(characterLength)|| characterLength < 8 || characterLength > 128) {
+    alert("Please enter character length as a number, between 8-128. Please try again.");
+    return false;
+  }
+  if (confirm("Would you like to include lowercase letters in the password?")) {
+    choiceArray = choiceArray.concat(lowerCase);
+  }
+  if (confirm("Would you like to include uppercase letters in the password?")) {
+    choiceArray = choiceArray.concat(upperCase);
+  }
+  if (confirm("Would you like to include special characters in the password?")) {
+    choiceArray = choiceArray.concat(specialCharacter);
+  }
+  if (confirm("Would you like to include numbers in the password?")) {
+    choiceArray = choiceArray.concat(numberArray);
+  }
+  return true;
 
 }
-
 // Don't touch above. Write all code below
 
 // function generatePassword () {
@@ -45,4 +71,4 @@ function writePassword() {
 // }
 
 // Add event listener to generate button. below calls the writePasswork function stared above. need to write the writePassword function. 
-generateBtn.addEventListener("click", writePassword);
+
